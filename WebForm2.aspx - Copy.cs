5953859bf96a12dcdf9a0465600653b123f1b9b8
bhupesh-abjima <97227019+bhupesh-abjima@users.noreply.github.com>
2022-03-02 10:57:07 +0530

    Add files via upload

---
+++ WebForm2.aspx - Copy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TestProject
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string CS= ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+
+            {
+
+                 SqlCommand cmd = new SqlCommand("Select Product, Name", con);
+                cmd.connection = con;
+                int totalrows = cmd.ExecuteQuery();
+                Response.Write("totalrows= "+ totalRowsAffected.ToString());
+
+          
+
+                
+
+        }
+    }
+}
---
+++ Webform1.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Adodemo 
+{
+    public partial class WebForm1: System.Web.UI.Page
+    {
+        protected void Page_load(object sender ,EventArgs e)
+        {
+
+            String CS = "Data source=.,database= Sample ;integrated sequrity = SSPI";
+            sqlconnection con = new sqlconnection(cs);
+            sqlCommand cmd = new sqlCommand("Select * from tblproduct", con);
+            Gridview1.datasource = cmd.ExecuteReader();
+            GridView1.databind();
+            con.close();
+        }
+    }
+    }
